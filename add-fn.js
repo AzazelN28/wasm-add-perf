31a44887481d849bec0e5d358ac1d2402879f5b5
AzazelN28 <asakon28@gmail.com>
2023-03-08 17:21:45 +0100

    change fn method (almost same results)

--- add-fn.js
+++ add-fn.js
@@ -2,18 +2,18 @@ function add(a, b) {
   return a + b
 }
 
+function compute(buffer) {
+  for (let i = 0; i < size; i += 3) {
+    buffer[i + 2] = add(buffer[i + 0], buffer[i + 1]);
+  }
+}
+
 const size = 2048 * 12
 const buffer = new Int32Array(size)
 for (let i = 0; i < size; i++) {
   buffer[i] = (Math.random() * 0xFFFF)
 }
 
-function compute() {
-  for (let i = 0; i < size; i += 3) {
-    buffer[i + 2] = add(buffer[i + 0], buffer[i + 1]);
-  }
-}
-
 console.time("compute")
-compute()
+compute(buffer)
 console.timeEnd("compute")
